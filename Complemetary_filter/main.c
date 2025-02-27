99d057d767b52f7e6eb81a42cbae8cd976acdf83
MomirCarevic <carevic.momir00@gmail.com>
2025-02-27 17:21:49 +0100

    Started with reading .csv file, just first few lines

--- Complemetary_filter/main.c
+++ Complemetary_filter/main.c
@@ -1,2 +1,10 @@
 #include "main.h"
 
+int main()
+{
+    FILE *in;
+    in = safe_open("../MPU_raw_data/MPU_data.csv","r",2);
+    readCSV(in);
+    return 0;
+
+}
--- Complemetary_filter/main.h
+++ Complemetary_filter/main.h
@@ -1,1 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+
+FILE *safe_open(char *name, char *mode, int n)
+{
+    FILE *file = fopen(name,mode);
+
+    if(file == NULL)
+    {
+       perror("Error loading the file!");
+       exit(n);
+    }
+
+    return file;
+}
+
+int readCSV(FILE *file)
+{
+    
+    char line[5];
+    int ctr = 0;
+
+    while (fgets(line,sizeof(line),file) && ctr < 10)    //  Read each line
+    {
+         char *token = strtok(line,",");
+
+         while(token != NULL)
+         {
+             printf("%s\n",token);
+             token = strtok(NULL,",");
+         }
+         printf("\n");
+         ctr++;
+    }
+    fclose(file);
+}
